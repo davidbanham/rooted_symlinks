bc324d93d2778df53fa67cec7d92fa9ce4503e41
David Banham <david@banham.id.au>
2015-12-09 11:37:14 +1100

    Initial commit

---
+++ foo.js
@@ -0,0 +1,1 @@
+console.log('And requiring through the root symlink works like a champ, too!');
---
+++ test.js
@@ -0,0 +1,5 @@
+var bandname = require('bandname');
+
+console.log(bandname()+'! installing packages and requiring them works just fine!');
+
+require('root/foo');

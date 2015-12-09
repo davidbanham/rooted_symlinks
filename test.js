c9f3c9f486fddaf7b4135af3ac268be014654e76
David Banham <david@banham.id.au>
2015-12-10 10:25:55 +1100

    Test subsequent installs

--- test.js
+++ test.js
@@ -3,3 +3,9 @@ var bandname = require('bandname');
 console.log(bandname()+'! installing packages and requiring them works just fine!');
 
 require('root/foo');
+
+var exec = require('child_process').execSync;
+
+exec('npm install random-string');
+var rand = require('random-string');
+console.log(rand()+' means that subsequent installs still work!');
